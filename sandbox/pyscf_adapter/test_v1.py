458e9ec588f61d165a825c3c08de4c3f55c5d3f3
nstair <nick_stair@yahoo.com>
2024-11-25 15:21:19 -0800

    Can build basic Mol object with pyscf.

---
+++ sandbox/pyscf_adapter/test_v1.py
@@ -0,0 +1,50 @@
+import qforte as qf
+
+print('\nBuild Geometry')
+print('-------------------------')
+
+symm_str = 'c1'
+
+geom = [('H', (0., 0., 1.00)),
+        ('H', (0., 0., 2.00)), 
+        ('H', (0., 0., 3.00)),
+        ('H', (0., 0., 4.00)),
+        # ('H', (0., 0., 5.00)),
+        # ('H', (0., 0., 3.00)), 
+        ]
+
+### ====> Build Qforte Mol with Psi4 <==== ###
+mol1 = qf.system_factory(
+    build_type='psi4', 
+    symmetry=symm_str,
+    mol_geometry=geom, 
+    basis='sto-6g', 
+    run_fci=True, 
+    run_ccsd=False,
+    store_mo_ints=True,
+    build_df_ham=False,
+    # df_icut=1.0e-1
+    )
+
+
+print(f'The FCI energy from Psi4:         {mol1.fci_energy:12.10f}')
+print(f'The SCF energy from Psi4:         {mol1.hf_energy:12.10f}')
+
+
+
+### ====> Build Qforte Mol with Psi4 <==== ###
+mol2 = qf.system_factory(
+    build_type='pyscf', 
+    symmetry=symm_str,
+    mol_geometry=geom, 
+    basis='sto-6g', 
+    run_fci=True, 
+    run_ccsd=False,
+    store_mo_ints=True,
+    build_df_ham=False,
+    # df_icut=1.0e-1
+    )
+
+
+print(f'The FCI energy from Pyscf:         {mol2.fci_energy:12.10f}')
+print(f'The SCF energy from Pyscf:         {mol2.hf_energy:12.10f}')
--- src/qforte/adapters/molecule_adapters.py
+++ src/qforte/adapters/molecule_adapters.py
@@ -24,6 +24,13 @@ try:
 except:
     use_psi4 = False
 
+try:
+    import pyscf
+    from pyscf import gto, scf, mp, fci, ao2mo, symm
+    use_pyscf = True
+except:
+    use_pyscf = False
+
 
 def create_psi_mol(**kwargs):
     """Builds a qforte Molecule object directly from a psi4 calculation.
@@ -142,7 +149,6 @@ def create_psi_mol(**kwargs):
     for row in orbitals:
         hf_orbital_energies.append(row[0])
         orb_irreps_to_int.append(row[1])
-    del orbitals
 
     point_group = p4_mol.symmetry_from_input().lower()
     irreps = qforte.irreps_of_point_groups(point_group)
@@ -437,6 +443,420 @@ def create_psi_mol(**kwargs):
 
     # Order Psi4 to delete its temporary files.
     psi4.core.clean()
+
+    return qforte_mol
+
+def create_pyscf_mol(**kwargs):
+    """Builds a qforte Molecule object directly from a pyscf calculation.
+
+    Returns
+    -------
+    Molecule
+        The qforte Molecule object which holds the molecular information.
+    """
+
+    kwargs.setdefault('symmetry', 'c1')
+    kwargs.setdefault('charge', 0)
+    kwargs.setdefault('multiplicity', 1)
+
+    mol_geometry = kwargs['mol_geometry']
+    basis = kwargs['basis']
+    multiplicity = kwargs['multiplicity']
+    charge = kwargs['charge']
+
+    qforte_mol = Molecule(
+        mol_geometry = mol_geometry,
+        basis = basis,
+        multiplicity = multiplicity,
+        charge = charge,
+        )
+
+    if not use_pyscf:
+        raise ImportError("PySCF was not imported correctely.")
+
+    # By default, the number of frozen orbitals is set to zero
+    kwargs.setdefault('num_frozen_docc', 0)
+    kwargs.setdefault('num_frozen_uocc', 0)
+
+    # run_scf is not read, because we always run SCF to get a wavefunction object.
+    kwargs.setdefault('run_mp2', False)
+    kwargs.setdefault('run_ccsd', False)
+    kwargs.setdefault('run_cisd', False)
+    kwargs.setdefault('run_fci', False)
+
+    # ===> Run PySCF End <=== #
+
+    pyscf_geom_str = ""
+
+    for geom_line in mol_geometry:
+        pyscf_geom_str += f"\n{geom_line[0]}  {geom_line[1][0]}  {geom_line[1][1]}  {geom_line[1][2]}"
+
+    print(' ==> PySCF geometry <==')
+    print('-------------------------')
+    print(pyscf_geom_str)
+
+    # Determine the spin (number of unpaired electrons)
+    spin = multiplicity - 1
+
+    # Create the molecule object
+    pyscf_mol = mol = gto.Mole()
+    pyscf_mol.build(
+        atom=pyscf_geom_str,
+        basis=basis,
+        charge=charge,
+        spin=spin,
+        symmetry = True,
+        symmetry_subgroup = kwargs.get('symmetry', 'c1')
+    )
+
+    # Determine the reference type
+
+    if multiplicity != 1:
+        raise ValueError(f"Unsupported multiplicity: {multiplicity}, currently unsupporded")
+
+    mf = scf.RHF(pyscf_mol)
+
+    # Set convergence options
+    mf.conv_tol = 1e-8          # Energy convergence criterion
+    mf.conv_tol_grad = 1e-8     # Density convergence criterion
+
+    # Perform the SCF calculation
+    pyscf_Escf = mf.kernel()
+    # pyscf_wfn = mf  # Wavefunction object analogous to Psi4's wfn
+
+    # Perform additional computations if requested
+    if kwargs.get('run_mp2', False):
+        num_frozen_docc = kwargs.get('num_frozen_docc', 0)
+        num_frozen_uocc = kwargs.get('num_frozen_uocc', 0)
+        nmo = mf.mo_coeff.shape[1]
+        nocc = mol.nelectron // 2
+
+        # Build list of frozen orbitals
+        frozen = []
+        if num_frozen_docc > 0:
+            frozen += list(range(num_frozen_docc))  # Freeze core orbitals
+        if num_frozen_uocc > 0:
+            frozen += list(range(nmo - num_frozen_uocc, nmo))  # Freeze virtual orbitals
+
+        if frozen:
+            mymp = mp.MP2(mf).set(frozen=frozen)
+        else:
+            mymp = mp.MP2(mf)
+        mp2_energy, _ = mymp.kernel()
+
+        # Store the MP2 energy
+        qforte_mol = lambda: None  # Placeholder for your data structure
+        qforte_mol.mp2_energy = mp2_energy
+
+    if kwargs.get('run_fci', False):
+        if kwargs.get('num_frozen_uocc', 0) == 0:
+            cisolver = fci.FCI(mf)
+            fci_energy, _ = cisolver.kernel()
+            qforte_mol.fci_energy = fci_energy
+        else:
+            print('\nWARNING: Skipping FCI computation due to frozen virtual orbitals not being supported in PySCF FCI.\n')
+
+    # Get the MO coefficients
+    C = mf.mo_coeff
+
+    # Get the scalar variables (nuclear repulsion energy)
+    pyscf_Enuc_ref = mol.energy_nuc()
+
+    # Get one-electron integrals and transform to MO basis
+    h_core = mf.get_hcore()
+    mo_oeis = C.T @ h_core @ C  # Transformed one-electron integrals
+
+    # Get two-electron integrals and transform to MO basis
+    nmo = C.shape[1]
+    # AO to MO transformation of two-electron integrals
+    mo_teis = ao2mo.kernel(mol, C, compact=False).reshape(nmo, nmo, nmo, nmo)
+
+    # Get the number of alpha and beta electrons
+    nalpha, nbeta = mol.nelec
+    nel = nalpha + nbeta
+
+    # ===> Run PySCF End <=== #
+
+    # ===> Get more PySCF info <=== #
+
+    # Retrieve the number of frozen core and virtual orbitals
+    frozen_core = kwargs.get('num_frozen_docc', 0)
+    frozen_virtual = kwargs.get('num_frozen_uocc', 0)
+
+    # Get symmetry information
+    point_group = mol.groupname.lower()
+    S = mol.intor('int1e_ovlp')
+    irreps_pyscf = symm.label_orb_symm(mol, mol.irrep_name, mol.symm_orb, C, S)
+    irreps_qforte = qforte.irreps_of_point_groups(point_group)
+    irrep_to_index = {irrep : idx for idx, irrep in enumerate(irreps_qforte)}
+
+    # Build the list of orbitals with their energies and irrep indices
+    orbitals = []
+    for i in range(len(mf.mo_energy)):
+        orbital_energy = mf.mo_energy[i]
+        irrep_name = irreps_pyscf[i]
+        irrep_idx = irrep_to_index[irrep_name]
+        orbitals.append([orbital_energy, irrep_idx])
+
+    # Sort orbitals by energy
+    orbitals.sort()
+
+    # Extract orbital energies and irrep indices
+    hf_orbital_energies = []
+    orb_irreps_to_int = []
+    for row in orbitals:
+        hf_orbital_energies.append(row[0])
+        orb_irreps_to_int.append(row[1])
+
+    # print('\n\n')
+    # print(f"Pyscf orbitals {orbitals}")
+    # print(f"Pyscf point_group {point_group}")
+    # print(f"Pyscf orb_irreps_to_int {orb_irreps_to_int}")
+    # print(f"Pyscf irreps {irreps_qforte}")
+    # print('\n\n')
+
+    orb_irreps = [irreps_qforte[i] for i in orb_irreps_to_int]
+
+
+    # Compute the frozen core energy
+    frozen_core_energy = 0.0
+
+    if frozen_core > 0:
+        # Sum over frozen core orbitals
+        for i in range(frozen_core):
+            frozen_core_energy += 2 * mo_oeis[i, i]
+
+        for i in range(frozen_core):
+            for j in range(frozen_core):
+                frozen_core_energy += 2 * mo_teis[i, i, j, j] - mo_teis[i, j, j, i]
+
+        # Adjust the one-electron integrals to account for frozen core orbitals
+        for p in range(frozen_core, nmo - frozen_virtual):
+            for q in range(frozen_core, nmo - frozen_virtual):
+                for i in range(frozen_core):
+                    mo_oeis[p, q] += 2 * mo_teis[p, q, i, i] - mo_teis[p, i, i, q]
+
+    # Build the Hartree-Fock reference configuration
+    num_active_orbitals = nmo - frozen_core - frozen_virtual
+    num_active_electrons = nel - 2 * frozen_core
+    hf_reference = [1] * num_active_electrons + [0] * (2 * num_active_orbitals - num_active_electrons)
+
+    # Build second quantized Hamiltonian
+    Hsq = qforte.SQOperator()
+    Hsq.add(pyscf_Enuc_ref + frozen_core_energy, [], [])
+    for i in range(frozen_core, nmo - frozen_virtual):
+        ia = (i - frozen_core)*2
+        ib = (i - frozen_core)*2 + 1
+        for j in range(frozen_core, nmo - frozen_virtual):
+            ja = (j - frozen_core)*2
+            jb = (j - frozen_core)*2 + 1
+
+            Hsq.add(mo_oeis[i,j], [ia], [ja])
+            Hsq.add(mo_oeis[i,j], [ib], [jb])
+
+            for k in range(frozen_core, nmo - frozen_virtual):
+                ka = (k - frozen_core)*2
+                kb = (k - frozen_core)*2 + 1
+                for l in range(frozen_core, nmo - frozen_virtual):
+                    la = (l - frozen_core)*2
+                    lb = (l - frozen_core)*2 + 1
+
+                    if(ia!=jb and kb != la):
+                        Hsq.add( mo_teis[i,l,k,j]/2, [ia, jb], [kb, la] ) # abba
+                    if(ib!=ja and ka!=lb):
+                        Hsq.add( mo_teis[i,l,k,j]/2, [ib, ja], [ka, lb] ) # baab
+
+                    if(ia!=ja and ka!=la):
+                        Hsq.add( mo_teis[i,l,k,j]/2, [ia, ja], [ka, la] ) # aaaa
+                    if(ib!=jb and kb!=lb):
+                        Hsq.add( mo_teis[i,l,k,j]/2, [ib, jb], [kb, lb] ) # bbbb
+
+    # ===> Get More PySCF Attributes END <=== #
+
+    # Set attributes
+    qforte_mol.nuclear_repulsion_energy = pyscf_Enuc_ref
+    qforte_mol.hf_energy = pyscf_Escf
+    qforte_mol.hf_reference = hf_reference
+    qforte_mol.sq_hamiltonian = Hsq
+    if kwargs['build_qb_ham']:
+        qforte_mol.hamiltonian = Hsq.jw_transform()
+    else:
+        Hsq.simplify()
+        qforte_mol.hamiltonian = None
+
+    qforte_mol.point_group = [point_group, irreps_qforte]
+    qforte_mol.orb_irreps = orb_irreps
+    qforte_mol.orb_irreps_to_int = orb_irreps_to_int
+    qforte_mol.hf_orbital_energies = hf_orbital_energies
+    qforte_mol.frozen_core = frozen_core
+    qforte_mol.frozen_virtual = frozen_virtual
+    qforte_mol.frozen_core_energy = frozen_core_energy
+
+    if kwargs['build_df_ham']:
+        if not kwargs['store_mo_ints']:
+            raise ValueError("store_mo_ints must be True if you want to build_df_ham")
+        else:
+            pyscf_mo_oeis = copy.deepcopy(mo_oeis)
+            pyscf_mo_teis = copy.deepcopy(mo_teis)
+
+    if kwargs['store_mo_ints']:
+
+        # keep ordering consistant with openfermion eri tensors
+        mo_teis = np.asarray(mo_teis.transpose(0, 2, 3, 1), order='C')
+
+        # need restricted version
+        h2e_rest = copy.deepcopy(np.einsum("ijlk", -0.5 * mo_teis))
+
+        # additoinal manipulation
+        h1e = copy.deepcopy(mo_oeis)
+        h2e = np.moveaxis(copy.deepcopy(h2e_rest), 1, 2) * (-1.0)
+        h1e -= np.einsum('ikkj->ij', h2e)
+
+        # just going to precumpute the einseum (for now)
+        h2e_einsum = copy.deepcopy(h2e + np.einsum('ijkl->klij', h2e))
+
+        # allocate qf tensors
+        qf_mo_oeis = qforte.Tensor(shape=np.shape(h1e), name='mo_oeis')
+        qf_mo_teis = qforte.Tensor(shape=np.shape(h2e), name='mo_teis')
+        qf_mo_teis_einsum = qforte.Tensor(shape=np.shape(h2e_einsum), name='mo_teis_einsum')
+        
+        # fill qf tensors
+        qf_mo_oeis.fill_from_nparray(h1e.ravel(), np.shape(h1e))
+        qf_mo_teis.fill_from_nparray(h2e.ravel(), np.shape(h2e)) 
+        qf_mo_teis_einsum.fill_from_nparray(
+            h2e_einsum.ravel(), 
+            np.shape(h2e_einsum)) 
+
+        qforte_mol.mo_oeis = qf_mo_oeis
+        qforte_mol.mo_teis = qf_mo_teis
+        qforte_mol.mo_teis_einsum = qf_mo_teis_einsum
+
+        # TODO(Nick), If we want better controll over this, it there shuld be a molecule member function that
+        # builds the df_ham from the stored mo_oeis and mo_teis rather than building it when pyscf 
+        # is initially run!
+        if kwargs['build_df_ham']:
+            raise NotImplementedError("WARNING: Building DF Hamiltonain using pyscf is not tested")
+            # keep ordering consistant with openfermion eri tensors
+            pyscf_mo_teis = np.asarray(pyscf_mo_teis.transpose(0, 2, 3, 1), order='C')
+
+            # do first factorization from integrals
+            ff_eigenvalues, one_body_squares, one_body_correction = first_factorization(
+                tei = pyscf_mo_teis,
+                lmax=None, # change if we want 
+                spin_basis=False,
+                threshold=kwargs['df_icut']) # may be very important to play with
+            
+            # do second factorization based on integrals and first factorizaiotn
+            scaled_density_density_matrices, basis_change_matrices = second_factorization(
+                ff_eigenvalues, 
+                one_body_squares)
+        
+            trotter_basis_change_matrices = [
+                # basis_change_matrices[0] @ expm(-1.0j * (p4_mo_oeis + one_body_correction[::2, ::2]))
+                np.zeros(shape=(nmo,nmo))
+            ]
+
+            for ii in range(len(basis_change_matrices) - 1):
+
+                trotter_basis_change_matrices.append(
+                    basis_change_matrices[ii + 1] @ basis_change_matrices[ii].conj().T)
+            
+            trotter_basis_change_matrices.append(basis_change_matrices[ii + 1].conj().T)
+
+            qf_ff_eigenvalues = qforte.Tensor(
+                shape=np.shape(ff_eigenvalues), 
+                name='first_factorization_eigenvalues')
+            
+            qf_one_body_squares = qforte.Tensor(
+                shape=np.shape(one_body_squares), 
+                name='one_body_squares')
+            
+            qf_one_body_ints = qforte.Tensor(
+                shape=np.shape(pyscf_mo_oeis), 
+                name='one_body_ints')
+            
+            qf_one_body_correction = qforte.Tensor(
+                shape=np.shape(one_body_correction[::2, ::2]), 
+                name='one_body_correction')
+            
+            qf_ff_eigenvalues.fill_from_nparray(
+                ff_eigenvalues.ravel(), 
+                np.shape(ff_eigenvalues))
+            
+            qf_one_body_squares.fill_from_nparray(
+                one_body_squares.ravel(), 
+                np.shape(one_body_squares))
+            
+            qf_one_body_ints.fill_from_nparray(
+                pyscf_mo_oeis.ravel(), 
+                np.shape(pyscf_mo_oeis))
+            
+            qf_one_body_correction.fill_from_nparray(
+                one_body_correction[::2, ::2].ravel(), 
+                np.shape(one_body_correction[::2, ::2]))
+            
+            # ===> convert lists of numpy arrays to lists of qforte Tensors
+
+            qf_scaled_density_density_matrices = []
+
+            for l in range(len(scaled_density_density_matrices)):
+            
+                qf_scaled_density_density_mat = qforte.Tensor(
+                    shape=np.shape(scaled_density_density_matrices[l]), 
+                    name=f'scaled_density_density_matrices_{l}')
+                
+                qf_scaled_density_density_mat.fill_from_nparray(
+                scaled_density_density_matrices[l].ravel(), 
+                np.shape(scaled_density_density_matrices[l]))
+
+                qf_scaled_density_density_matrices.append(
+                    qf_scaled_density_density_mat
+                )
+            
+
+            qf_basis_change_matrices = []
+
+            for l in range(len(basis_change_matrices)):
+                qf_basis_change_mat = qforte.Tensor(
+                    shape=np.shape(basis_change_matrices[l]), 
+                    name=f'basis_change_matrices_{l}')
+                
+                qf_basis_change_mat.fill_from_nparray(
+                    basis_change_matrices[l].ravel(), 
+                    np.shape(basis_change_matrices[l]))
+            
+                qf_basis_change_matrices.append(
+                    qf_basis_change_mat
+                )
+
+            
+            qf_trotter_basis_change_matrices = []
+            for l in range(len(trotter_basis_change_matrices)):
+
+                qf_trotter_basis_change_mat = qforte.Tensor(
+                    shape=np.shape(trotter_basis_change_matrices[l]), 
+                    name=f'trotter_basis_change_matrices_{l}')
+                
+                
+                qf_trotter_basis_change_mat.fill_from_nparray(
+                    trotter_basis_change_matrices[l].ravel(), 
+                    np.shape(trotter_basis_change_matrices[l]))
+                
+                qf_trotter_basis_change_matrices.append(
+                    qf_trotter_basis_change_mat
+                )
+
+            qforte_mol._df_ham = qforte.DFHamiltonian(
+                nel=nel,
+                norb=nmo,
+                eigenvalues = qf_ff_eigenvalues,
+                one_body_squares = qf_one_body_squares,
+                one_body_ints = qf_one_body_ints,
+                one_body_correction = qf_one_body_correction,
+                scaled_density_density_matrices = qf_scaled_density_density_matrices,
+                basis_change_matrices = qf_basis_change_matrices,
+                trotter_basis_change_matrices = qf_trotter_basis_change_matrices
+            )
 
     return qforte_mol
 
