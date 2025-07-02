8bb91320ec9cf150ed558b2d9e9dad9287e6c8b2
Zach_Gonzales <zach_gonzales@pop-os.localdomain>
2025-07-02 11:56:37 -0700

    init thrust tensor working

--- src/qforte/tensor_thrust.h
+++ src/qforte/tensor_thrust.h
@@ -20,14 +20,15 @@ public:
 
     void zero();
 
-    std::vector<std::complex<double>>& get_host_data();     // Returns a standard representation of the data on the host
-    std::string get_name();                                 // Returns the name of the tensor
-    size_t get_ndim();                                      // Returns the number of dimensions of the tensor
-    size_t get_size();                                      // Returns the total number of elements in the tensor
-    double get_norm();                                      // Returns the norm of the tensor
-    const std::vector<size_t>& shape() const;               // Returns size in each dimension
-    const bool initialized() const;                         // Returns true if the tensor has been initialized
-    void TensorGPUThrust::update_strides();                 // Updates the strides based on the shape of the tensor
+    std::vector<std::complex<double>> get_data() const;     // Returns a standard representation of the data on the host
+    std::string get_name() const { return name_; }            // Returns the name of the tensor
+    size_t ndim() const { return shape_.size(); }            // Returns the number of dimensions of the tensor
+    size_t size() const { return size_; }                    // Returns the total number of elements in the tensor
+    const std::vector<size_t>& shape() const { return shape_; }  // Returns size in each dimension
+    bool initialized() const { return initialized_; }         // Returns true if the tensor has been initialized
+    void update_strides();                 // Updates the strides based on the shape of the tensor
+
+    void resize(const std::vector<size_t>& new_shape); // Resizes the tensor to the new shape, preserving data if possible
 
     void set(const std::vector<size_t>&, 
              const std::complex<double>
@@ -35,15 +36,9 @@ public:
     
     void fill_from_nparray(std::vector<std::complex<double>>, std::vector<size_t>);
 
-    void TensorGPUThrust::ndim_error(const std::vector<size_t>&) const; // Checks if the number of indices matches the number of dimensions
+    void ndim_error(const std::vector<size_t>&) const; // Checks if the number of indices matches the number of dimensions
 
-    std::string str(
-        bool print_data = true,
-        bool print_complex = false,
-        int maxcols = 6,
-        const std::string& data_format = "%12.7f",
-        const std::string& header_format = "%12zu"
-    ) const;
+    std::string str() const; // Returns a string representation of the tensor
 
 
 
@@ -56,5 +51,5 @@ private:
     bool on_gpu_;
 
     thrust::host_vector<thrust::complex<double>> h_data_;
-    thrust::device_vector<thrust::complex<double>> d_vec_;
+    thrust::device_vector<thrust::complex<double>> d_data_;
 };
