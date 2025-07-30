3ebf7ae6099287daa75c97edd94be5e50a6c42c4
ZachVGonzales <ZachVGonzales@gmail.com>
2025-07-30 10:36:14 -0700

    working v2 make_each

--- src/qforte/fci_graph_thrust.h
+++ src/qforte/fci_graph_thrust.h
@@ -79,6 +79,15 @@ public:
         thrust::device_vector<int>& source,
         thrust::device_vector<int>& target,
         thrust::device_vector<cuDoubleComplex>& parity);
+    
+    void make_mapping_each_gpu_v2(
+        bool alpha, 
+        const std::vector<int>& dag, 
+        const std::vector<int>& undag,
+        int* count,
+        thrust::device_vector<int>& source_gpu,
+        thrust::device_vector<int>& target_gpu,
+        thrust::device_vector<cuDoubleComplex>& parity_gpu);
 
     /// ==> Utility Functions for Bit Math (may need to move) <== ///
 
